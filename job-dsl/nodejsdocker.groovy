629c2613410a9c346d2d2787a35e5b12930f4437
Nagachandra <71181983+pchandragithub@users.noreply.github.com>
2021-09-14 10:41:13 +0530

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,8 +1,8 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/pchandragithub/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('pchandra.pavuluri@gmail.com')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
     }
     triggers {
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('pchandragithub/docker-nodejs-demo')
+            repositoryName('mydockermagic/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
